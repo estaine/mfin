44e3823124abe5babf78503ea4a7ce3d0b94c9c2
Andrey Rykhalski <andrey.rykhalski@dmetrics.com>
2017-02-16 13:56:37 +0300

    Tag entity and DB table added

---
+++ src/main/java/com/estaine/mfin/dao/TagRepository.java
@@ -0,0 +1,7 @@
+package com.estaine.mfin.dao;
+
+import com.estaine.mfin.model.Tag;
+import org.springframework.data.repository.Repository;
+
+public interface TagRepository extends Repository<Tag, Integer> {
+}
---
+++ src/main/java/com/estaine/mfin/model/Tag.java
@@ -0,0 +1,16 @@
+package com.estaine.mfin.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+public class Tag {
+    @Id
+    @GeneratedValue
+    @Getter private Integer id;
+
+    @Getter @Setter private String name;
+}
